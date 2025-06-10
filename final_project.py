e74ee246444ff3139ebab8cb2563989d9fcca435
Ohad Sheelo <71489867+Ohad25@users.noreply.github.com>
2025-06-10 14:41:49 +0300

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -132,16 +132,14 @@ def create_spectogram(data1,data2, fs, channel,difference=False,baseline=False):
 
     #Spectogram for left and right channels  #TODO: mean over all spectogram.  
     elif baseline:    
-        baseline_mask = t < 2 #The assisngment says to use the first second as baseline but we start at 1 seconds
+        baseline_mask = t <= 1 #The assisngment says to use the first second as baseline but we start at 1 seconds
         #Average across time baseline period:
-        baseline_left = np.mean(Sxx_left[:, :,baseline_mask], axis=1) 
-        baseline_right = np.mean(Sxx_right[:, :,baseline_mask], axis=1)
-        #Average across trials:
-        trail_baseline_left = np.mean(baseline_left, axis=0) 
-        trail_baseline_right = np.mean(baseline_right, axis=0) 
+        baseline_Sxx_left = np.mean(Sxx_left[:,baseline_mask])  # Select the first second of the spectrogram
+        baseline_Sxx_right = np.mean(Sxx_right[:,baseline_mask])  # Select the first second of the spectrogram
 
-        difference_Sxx_left = Sxx_left - trail_baseline_left
-        difference_Sxx_right = Sxx_right - trail_baseline_right
+
+        difference_Sxx_left = Sxx_left - baseline_Sxx_left
+        difference_Sxx_right = Sxx_right - baseline_Sxx_right
         data_Sxx = [(difference_Sxx_left,'Left (Substraction from baseline (1 [sec]))'), (difference_Sxx_right,'Right (Substraction from baseline (1 [sec]))')]
 
     #Baseline period
@@ -165,8 +163,40 @@ def create_spectogram(data1,data2, fs, channel,difference=False,baseline=False):
     plt.show()
     
 
+def extract_features(data1,data2, fs, channel,frequancy_band1,frequancy_band2,st_fix,en_fix):
+    """
+    data1: signal of left hand movement
+    data2: signal of right hand movement
+    fs: sampling frequency
+    channel: channel index to analyze (0 for C3, 1 for C4)
+    frequancy_band1: tuple of (start_freq, end_freq) for the first frequency band
+    frequancy_band2: tuple of (start_freq, end_freq) for the second frequency band
+    st_fix: start fixation time in seconds
+    en_fix: end fixation time in seconds
+    """
+    trial_start = int(st_fix * fs)
+    trial_end = int(en_fix * fs)
+    
+    signal_left = data1[:, trial_start:trial_end, channel]  # C3 channel
+    signal_right = data2[:, trial_start:trial_end, channel]  # C4 channel
 
+    # Calculate the power spectrum for the left hand movement
+    f_left, pxx_left = welch(signal_left, fs, nperseg=256,noverlap=128,axis=1)
+    
+    # Calculate the power spectrum for the right hand movement
+    f_right, pxx_right = welch(signal_right, fs, nperseg=256,noverlap=128,axis=1)
 
+    # Extract features for the specified frequency bands
+    band1_mask_left = (f_left >= frequancy_band1[0]) & (f_left <= frequancy_band1[1])
+    band2_mask_left = (f_left >= frequancy_band2[0]) & (f_left <= frequancy_band2[1])
+    
+    band1_mask_right = (f_right >= frequancy_band1[0]) & (f_right <= frequancy_band1[1])
+    band2_mask_right = (f_right >= frequancy_band2[0]) & (f_right <= frequancy_band2[1])
+
+    feature_vector_left = np.array([np.mean(pxx_left[band1_mask_left]), np.mean(pxx_left[band2_mask_left])])
+    feature_vector_right = np.array([np.mean(pxx_right[band1_mask_right]), np.mean(pxx_right[band2_mask_right])])
+
+    return feature_vector_left, feature_vector_right
 
 
 
