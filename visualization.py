9c215f66fd621839047504d51ac532a47f16e19b
Ohad Sheelo <71489867+Ohad25@users.noreply.github.com>
2025-06-30 14:07:43 +0300

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -11,15 +11,15 @@ def plot_EEG(data, fs, title, channels_idx, side):
     for i in range(20):
         ax = axes[i // 5, i % 5]
         random_index = random_indices[i]
-        ax.plot(np.arange(data.shape[1]) / fs, data[random_index, :, c3_idx], label='C3', color='blue')
-        ax.plot(np.arange(data.shape[1]) / fs, data[random_index, :, c4_idx], label='C4', color='red')
+        ax.plot(np.arange(data.shape[1]) / fs, data[random_index, :, c3_idx], color='blue',alpha=0.9)
+        ax.plot(np.arange(data.shape[1]) / fs, data[random_index, :, c4_idx], color='red',alpha=0.7)
         ax.set_title(f'Trial {random_index}')
-        ax.set_xlabel('Time (s)')
-        ax.set_ylabel('Voltage (µV)')
+        ax.set_xlabel('Time [s]')
+        ax.set_ylabel('Voltage [µV]')
         ax.grid(True)
-        ax.legend()
 
-    plt.suptitle(f'EEG Data from {title} - {side} - Channels C3 & C4', fontsize=16)
+
+    plt.suptitle(f'EEG Data from {title} - {side} - Channels C3 (blue) & C4 (red)', fontsize=16)
     plt.tight_layout(rect=(0, 0.03, 1, 0.95))
     plt.show()
 
